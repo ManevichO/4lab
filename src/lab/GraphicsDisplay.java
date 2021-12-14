2b21832458c24a77c5a3e967e9e6e71ffa053860
Oleg Manevich <oleglmao@gmail.com>
2021-12-14 03:18:31 +0300

    3 commit

--- src/lab/GraphicsDisplay.java
+++ src/lab/GraphicsDisplay.java
@@ -1,4 +1,44 @@
 package lab;
 
-public class GraphicsDisplay {
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Paint;
+import java.awt.Stroke;
+import java.awt.font.FontRenderContext;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.GeneralPath;
+import java.awt.geom.Line2D;
+import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.text.NumberFormat;
+import java.util.LinkedList;
+import javax.swing.*;
+
+@SuppressWarnings("serial")
+public class GraphicsDisplay extends JPanel {
+    private Double[][] graphicsData;
+    private boolean showAxis = true;
+    private boolean showMarkers = true;
+    private double minX;
+    private double maxX;
+    private double minY;
+    private double maxY;
+    private double scale;
+    private BasicStroke graphicsStroke; // различные стили черчения линий
+    private BasicStroke axisStroke;
+    private boolean turnGraph = false;
+    private BasicStroke markerStroke;
+    private Font axisFont; // различные шрифты отображения надписей
 }
+    public GraphicsDisplay() {
+        setBackground(Color.WHITE);
+        graphicsStroke = new BasicStroke(2.0f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_ROUND, 10.0f, new float[] {10, 10, 10, 10, 10, 10, 30 , 30, 30,30,30,30}, 0.0f); // перо для рисования графика
+        axisStroke = new BasicStroke(2.0f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_MITER, 10.0f, null, 0.0f); // перо для рисования осей координат
+        markerStroke = new BasicStroke(1.0f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_MITER, 10.0f, null, 0.0f); // перо для рисования контуров маркеров
+        axisFont = new Font("Serif", Font.CENTER_BASELINE, 36); // шрифт для подписей осей координат
+    }
--- src/lab/Main.java
+++ src/lab/Main.java
@@ -1,8 +1,44 @@
 package lab;
 
-public class Main {
+import java.awt.BorderLayout;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+import javax.swing.JCheckBoxMenuItem;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JOptionPane;
+import javax.swing.event.MenuEvent;
+import javax.swing.event.MenuListener;
 
-    public static void main(String[] args) {
-	// write your code here
+@SuppressWarnings("serial")
+public class Main extends JFrame {
+    private static final int WIDTH = 800;
+    private static final int HEIGHT = 600;
+
+    private JFileChooser fileChooser = null;
+    private JCheckBoxMenuItem showAxisMenuItem;
+    private JCheckBoxMenuItem showMarkersMenuItem;
+    private GraphicsDisplay display = new GraphicsDisplay();
+    private boolean fileLoaded = false;
+
+    public Main() {
+        super("Построение графиков функций на основе заранее подготовленных файлов");
+        setSize(WIDTH, HEIGHT);
+        Toolkit kit = Toolkit.getDefaultToolkit();
+        setLocation((kit.getScreenSize().width - WIDTH) / 2, (kit.getScreenSize().height - HEIGHT) / 2);
+
+        JMenuBar menuBar = new JMenuBar();
+        setJMenuBar(menuBar);
+        JMenu fileMenu = new JMenu("Файл");
+        menuBar.add(fileMenu);
     }
 }
